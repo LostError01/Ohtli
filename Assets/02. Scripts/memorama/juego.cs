95776c744f97b50f27ee4112c284dda5e6800d46
gmatxo <davidfainsod@gmail.com>
2025-08-11 15:57:27 -0600

    mas cosas sobre cartas

--- Assets/02. Scripts/memorama/juego.cs
+++ Assets/02. Scripts/memorama/juego.cs
@@ -1,6 +1,7 @@
 
 using Ink.Parsed;
 using System.Collections.Generic;
+using System.Collections;
 using Unity.Mathematics;
 using Unity.VisualScripting.Antlr3.Runtime.Tree;
 using UnityEngine;
@@ -14,9 +15,10 @@ public class juego : MonoBehaviour
     [SerializeField]
     private int pares = 8;
     private List<control> _cartas = new List<control>();
-    private int columnas =4;
-    private int filas =4;
-     
+    private int columnas = 4;
+    private int filas = 4;
+    private control carta1;
+
     public void start()
     {
         if (pares > 8)
@@ -24,15 +26,15 @@ public class juego : MonoBehaviour
             Debug.Assert(false);
             pares = math.min(pares, 8);
         }
-        List <int> todos = new List<int>();
-        for(int i=0; i<8; ++i)
+        List<int> todos = new List<int>();
+        for (int i = 0; i < 8; ++i)
         {
             todos.Add(i);
         }
-        List <int> gametodos = new List<int>();
-        for(int i =0; i<pares; ++i)
+        List<int> gametodos = new List<int>();
+        for (int i = 0; i < pares; ++i)
         {
-            int escoger = todos[UnityEngine.Random.Range(0,todos.Count)];
+            int escoger = todos[UnityEngine.Random.Range(0, todos.Count)];
             todos.Remove(escoger);
             gametodos.Add(escoger);
             gametodos.Add(escoger);
@@ -43,31 +45,58 @@ public class juego : MonoBehaviour
         Vector3 offset = new Vector3((columnas - 1) * tamaño, (filas - 1) * tamaño, 0) * 0.5f;
         for (int i = 0; i < filas; i++)
         {
-            for(int j = 0; j < columnas; j++)
+            for (int j = 0; j < columnas; j++)
             {
                 Vector3 posicion = new Vector3(i * tamaño, j * tamaño, 0f);
-        var card = Instantiate(_Prefab, posicion - offset, Quaternion.identity);
-                card.tipo = gametodos[UnityEngine.Random.Range(0,gametodos.Count)];
+                var card = Instantiate(_Prefab, posicion - offset, Quaternion.identity);
+                card.tipo = gametodos[UnityEngine.Random.Range(0, gametodos.Count)];
                 gametodos.Remove(card.tipo);
                 card.OnClicked.AddListener(oncardcliked);
                 _cartas.Add(card);
-                
+
             }
-                
+
         }
     }
     public void oncardcliked(control card)
     {
-        card.test();
+        if (carta1 == null)
+        {
+            seleccionar(card);
+            return;
+        }
+        if (card.tipo == carta1.tipo)
+        {
+            StartCoroutine(acertar(card));
+            return;
+        }
+        StartCoroutine(fallar(card));
+    }
+    private void seleccionar(control card)
+    {
+        carta1 = card;
+        carta1.Revelar();
+    }
+    private IEnumerator acertar(control card)
+    {
+        card.Revelar();
+        yield return new WaitForSeconds(1f);
+        _cartas.Remove(carta1);
+        _cartas.Remove(card);
+        Destroy(card);
+        Destroy(carta1);
+        carta1 = null;
+    }
+    private IEnumerator fallar(control card)
+    {
+        card.Revelar();
+        yield return new WaitForSeconds(1f);
+        carta1.esconder();
+        card.esconder();
+        carta1 = null;
     }
     void Start() 
     {   
         start();
-    }
-
-    
-    void Update()
-    {
-        
     }
 }
